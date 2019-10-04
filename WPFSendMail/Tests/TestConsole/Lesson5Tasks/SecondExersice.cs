b084a3591b724b56f74db11ae215b3fb0b028e12
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-10-04 17:30:06 +0300

    Факториал и парсинг csv в потоках

---
+++ WPFSendMail/Tests/TestConsole/Lesson5Tasks/SecondExersice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole.Lesson5Tasks
+{
+    public static class SecondExersice
+    {
+        static List<String> _buffer = new List<string>();
+        static string FileInputPath { get; set; }
+        static string FileOutoutPath { get; set; }
+        static readonly object __SyncRoot = new object();
+        static bool _readDone = false;
+        public static void DoExercise(string fileInputPath, string fileOutputPath)
+        {
+            if (!File.Exists(fileInputPath)) return;
+            FileInputPath = fileInputPath;
+            FileOutoutPath = fileOutputPath;
+            Thread csvReadThread = new Thread(CSVRead) { IsBackground = true };
+            Thread txtWriteThread = new Thread(TXTWrite) { IsBackground = true };
+            Console.WriteLine("Парсинг начинаем из {0} в {1}", FileInputPath, FileOutoutPath);
+            csvReadThread.Start();
+            txtWriteThread.Start();
+            csvReadThread.Join();
+            txtWriteThread.Join();
+            Console.WriteLine("Парсинг закончен");
+        }
+        static void CSVRead()
+        {
+            using (StreamReader sr = new StreamReader(FileInputPath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    lock (__SyncRoot)
+                    {
+                        _buffer.Add(line);
+                    }
+                }
+                _readDone = true;
+            }
+        }
+        static void TXTWrite()
+        {
+            using (StreamWriter sw = new StreamWriter(FileOutoutPath))
+            {
+                string line;
+                while(!_readDone|| _buffer.Count > 0)
+                {
+                    if (_buffer.Count > 0)
+                        lock (__SyncRoot)
+                        {
+                            line = _buffer[0];
+                            _buffer.RemoveAt(0);
+                            sw.WriteLine(line);
+                        }
+                }
+            }
+        }
+    }
+}
