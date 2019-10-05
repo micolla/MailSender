e83ee5f321698fa521be3d569b0deac8a2871712
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-10-05 05:57:01 +0300

    Revert "Факториал и парсинг csv в потоках"
    This reverts commit b084a3591b724b56f74db11ae215b3fb0b028e12.

--- WPFSendMail/Tests/TestConsole/Program.cs
+++ WPFSendMail/Tests/TestConsole/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Collections;
 using System.Linq;
-using TestConsole.Lesson5Tasks;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -10,10 +11,14 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            FirstExercise.DoExercise();
-            SecondExersice.DoExercise("Lesson5Tasks/testdata.csv", "Lesson5Tasks/outputData.txt");
+            object i = "dsds";
+            Console.WriteLine(i.GetType().ToString());
+            IEnumerable si = new List<int> { 1,2};
+            foreach (var item in si)
+            {
+                Console.WriteLine(item.GetType());
+            }
             Console.ReadLine();
-
         }
     }
 }
