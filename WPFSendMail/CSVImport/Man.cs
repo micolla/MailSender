aae7ceb0450b3f1bb3e0c8beb83287b49a67b8b3
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-10-11 15:58:42 +0300

    Create CSVImport to DB

---
+++ WPFSendMail/CSVImport/Man.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVImport
+{
+    public class Man
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [MaxLength(150)]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Man() { }
+        public Man(string name, string email,string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
---
+++ WPFSendMail/CSVImport/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVImport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVReader reader = new CSVReader("Files\\ImportData.csv", ',');
+            List<Man> men = reader.ReadCSVMenAsync().Result;
+            using(var _db=new PeopleDbContext())
+            {
+                _db.Men.AddRange(men);
+                _db.SaveChanges();
+            }
+            Console.WriteLine("Проимпортировано");
+            Console.Read();
+        }
+    }
+}
