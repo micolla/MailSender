59f518e05f0073bd608fd9a3af0f7046600c024f
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-10-12 10:41:36 +0300

    Create Migrations

---
+++ WPFSendMail/MailSender.Lib/Data/EF/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace MailSender.Lib.Data.EF.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MailSender.Lib.Data.EF.MailSenderDB>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Data\EF\Migrations";
+        }
+
+        protected override void Seed(MailSender.Lib.Data.EF.MailSenderDB context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
